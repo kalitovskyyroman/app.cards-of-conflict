940abc567b425001fd21d1f7566cb4cc3f24b8b4
Roman Kalitovskyy <roman.kalitovskyy@gmail.com>
2023-02-09 17:12:11 +0200

    fix colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -25,16 +25,16 @@ const buttonStyles: OverridesStyleRules = {
             color: colors['soft-white'],
             backgroundColor: colors['soft-black'],
             boxShadow: `
-                7px -1px 1px 1px ${colors['chromatic-blue']},
-                -7px 1px 1px 1px ${colors['chromatic-red']}
+                5px -1px 1px 1px ${colors['chromatic-blue']},
+                -5px 1px 1px 1px ${colors['chromatic-red']}
             `,
         },
         ':active': {
             color: colors['soft-white'],
             backgroundColor: colors['soft-black'],
             boxShadow: `
-                7px -1px 1px 1px ${colors['chromatic-blue']},
-                -7px 1px 1px 1px ${colors['chromatic-red']}
+                -5px 1px 1px 1px ${colors['chromatic-red']},
+                5px -1px 1px 1px ${colors['chromatic-blue']}
             `,
         }
     },
